6dd473c016d32607844481cdd41b2fe17b16cb87
Zebra385 <houcheserge@gmail.com>
2020-02-05 18:09:26 +0100

    Ajout lecture fichier categorie en json

--- Main.py
+++ Main.py
@@ -1,32 +1,43 @@
-# coding: utf-8 # For French language
+# -*- coding: utf-8 -*-# For French language
+import json
 import sys
 from sqlalchemy import create_engine, Column, Integer, Text, MetaData, Table, select
 import requests
 
+
+class Categorie:
+    def __init__(self, **key_attribut):
+        for attr_name, attr_value in key_attribut.items():
+            setattr(self, attr_name, attr_value)
+            # print(attr_name,attr_value)
+
+
 """ We def a function to make a menu automaticly"""
+
 
 def display_menu(options):
     print("Bonjour, Bienvenue sur OFF_Substitut:")
     while True:
-        for idx,option in enumerate(options):
-            print("{} - {}".format(idx+1,option))
-        choice =input("Quel est votre choix")
+        for idx, option in enumerate(options):
+            print("{} - {}".format(idx+1, option))
+        choice = input("Quel est votre choix")
         try:
-            option_nb= int(choice)
-        except ValueError: # On anticipe l'erreur
+            option_nb = int(choice)
+        except ValueError:  # On anticipe l'erreur
             print("Non, mauvais choix, vous vous êtes trompé")
-            continue # retourne au debut de la boucle
+            continue  # retourne au debut de la boucle
 
         if 0 < option_nb <= len(options):
             return option_nb
         print("Non, mauvais choix, vous vous êtes trompé")
 
+
 """ We use qslalchely t create a table to downlaod  the substitut of food"""
 
-#We must connect to base de données
+# We must connect to base de données
 engine = create_engine('sqlite://')
 
-#We def and create our table
+# We def and create our table
 
 metadata = MetaData()
 messages = Table(
@@ -43,23 +54,26 @@ if __name__ == '__main__':
     insert_message = messages.insert().values(message='Nutela')
     # puis on excecute la requete
     engine.execute(insert_message)
-    choice= display_menu([
+    choice = display_menu([
         "Quel aliment souhaitez-vous remplacer ?",
         "Retrouver mes aliments substitués",
         "Quitter menu"
     ])
-    if choice== 1:
+    if choice == 1:
         print("option 1")
-        r = requests.get('https://fr.wiki.openfoodfacts.org/API/categories')
-        print(r.headers) # affiche  le  contenu des headers
-        print(r.headers['server'])# affiche le server
-    elif choice == 2 :
+        # r = requests.get('https://fr.wiki.openfoodfacts.org/API/categories')
+        # print(r.headers) # affiche  le  contenu des headers
+        # print(r.headers['server'])# affiche le server
+        for key_attribut in json.load(open("Categories_Aliment_France_OFF.json")):
+            # , "r",  encoding="UTF-8")):
+            categorie = Categorie(**key_attribut)
+            # print(categorie)
+    elif choice == 2:
         print("option 2")
         # interogeons nos données
 
         stmt = select([messages.c.message])
         message, = engine.execute(stmt).fetchone()
         print(message)
-    elif choice == 3 :
-        sys.exit()#permet de quiter le programme
-
+    elif choice == 3:
+        sys.exit()  # permet de quiter le programme
--- README.md
+++ README.md
@@ -11,10 +11,17 @@
 ## Celui-ci propose un menu  via le terminale: 
 ### Bonjour, Bienvenue sur OFF_Substitut:
 ### 1 - Quel aliment souhaitez-vous remplacer ?
-###2 - Retrouver mes aliments substitués
-###3 - Quitter menu
+### 2 - Retrouver mes aliments substitués
+### 3 - Quitter menu
 ### Quel est votre choix
 ## 4- La strastégie de develepement du projet:
 ### 4-1 fichier principale en python, main.py
 #### Automatisation à l'aide d'une fontion (displa_menu(option)) pour l'affichage
 #### d'un menu, option étant l'intitulé du numero(index) dans le menu 
+### 4-2 Comment récupérer les catégories dans openfoodfacts
+#### On utilise postman pour récuperer sous forme d'un fichier json nommé 
+#### Categories_Aliment_France_OFF.json les catégories. La requete GEST est :
+#### https://fr.openfoodfacts.org/categories.json
+#### Puis on enregistre le résultat  en cliquand sur "save reponse",
+#### il faut mettre le fichier dans le même réêrtoire que le programme
+####  principal Main.py
